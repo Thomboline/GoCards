4a44ab34f7096eba78955578ec6e55facd993f1c
Thomboline <Thomaslienchristensen@gmail.com>
2018-06-25 13:55:35 +0200

    Added the first receiver function.

--- main.go
+++ main.go
@@ -1,20 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// var card string = "Ace of Spades"
-	cards := []string{"Five of Diamonds", "Ace of Spades"}
+	cards := deck{"Five of Diamonds", "Ace of Spades"}
 
 	//When appending, Go does not just add another variable to the slice.
 	//It instead creates a new slice from the specified data
 	cards = append(cards, "Two of Hearts")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 
 }
 
