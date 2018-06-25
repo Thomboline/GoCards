3990eaf993f80fb75cbcb866c2544ace9d47fb17
Thomboline <Thomaslienchristensen@gmail.com>
2018-06-25 14:32:53 +0200

    Created newDeck() function.

--- deck.go
+++ deck.go
@@ -9,8 +9,27 @@ import (
 **/
 type deck []string
 
+//Receiver function. Receiver is of type deck
+//By convention, use a one-letter abreviation of the type
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func newDeck() deck {
+
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	//Using _ as an index variable tells Go, that you don't want to actually use the index variable
+	//Go will display an error if the index variable is not used, unless it's an _
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
 }
--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
 func main() {
-	// var card string = "Ace of Spades"
-	cards := deck{"Five of Diamonds", "Ace of Spades"}
-
-	//When appending, Go does not just add another variable to the slice.
-	//It instead creates a new slice from the specified data
-	cards = append(cards, "Two of Hearts")
-
+	cards := newDeck()
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
