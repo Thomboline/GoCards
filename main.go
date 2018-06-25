794487fd0c49be7e0af9a362202f446cc4ac5606
Thomboline <Thomaslienchristensen@gmail.com>
2018-06-25 13:24:22 +0200

    Begun lesson. Variables, slices and for-loops.

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	// var card string = "Ace of Spades"
+	cards := []string{"Five of Diamonds", "Ace of Spades"}
+
+	//When appending, Go does not just add another variable to the slice.
+	//It instead creates a new slice from the specified data
+	cards = append(cards, "Two of Hearts")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
